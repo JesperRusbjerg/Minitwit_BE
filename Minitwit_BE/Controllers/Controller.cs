8bf72de0a9b3cbcfb07808bb44df738fd487336c
Szymon Gabryel <szymonso1009@gmail.com>
2022-02-07 13:58:21 +0100

    Example c# project structure

---
+++ Minitwit_BE/Controllers/Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinitwitBE.Api.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class Controller : ControllerBase
+    {
+        [HttpGet("test")]
+        public async Task<string> TestEndpoint()
+        {
+            return await Task.FromResult("test");
+        }
+    }
+}

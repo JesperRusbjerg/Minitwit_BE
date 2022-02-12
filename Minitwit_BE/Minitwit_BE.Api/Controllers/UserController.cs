5129e955c987e54b565fdc7cc55516966b5c9164
Szymon Gabryel <szymonso1009@gmail.com>
2022-02-12 19:20:58 +0100

    Added logging, error handling and created a test project.

--- Minitwit_BE/Minitwit_BE.Api/Controllers/UserController.cs
+++ Minitwit_BE/Minitwit_BE.Api/Controllers/UserController.cs
@@ -9,17 +9,23 @@ namespace Minitwit_BE.Api.Controllers
     public class UserController : ControllerBase
     {
         private readonly TwitContext _twitContext;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(TwitContext twitContext)
+        public UserController(TwitContext twitContext, ILogger<UserController> logger)
         {
             _twitContext = twitContext;
+            _logger = logger;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<User>> registerUser([FromBody]UserInput input)
+        public async Task<ActionResult<User>> RegisterUser([FromBody]UserInput input)
         {
+            _logger.LogInformation($"RegisterUser endpoint was called for user: {input.UserName}");
+
             var existingUser = _twitContext.Users.FirstOrDefault(user => user.UserName.Equals(input.UserName) || user.Email.Equals(input.Email));
-            if (existingUser != null) {
+            
+            if (existingUser != null) 
+            {
                 return BadRequest("User with that nickname or email already exists!");
             }
 
@@ -32,21 +38,18 @@ namespace Minitwit_BE.Api.Controllers
 
             _twitContext.Add(user);
             _twitContext.SaveChanges();
+
             return Ok(user);
         }
 
         [HttpGet("login")]
-        public async Task<ActionResult<int>> login([FromBody]UserInput input)
+        public async Task<ActionResult<int>> Login([FromBody]UserInput input)
         {
+            _logger.LogInformation($"Login endpoint was called for user: {input.Email}");
+
             var authUser = _twitContext.Users.FirstOrDefault(user => user.Email.Equals(input.Email) && user.PwHash.Equals(input.PwHash));
-            if (authUser != null)
-            {   
-                return Ok(authUser.UserId);
-            }
-            else
-            {
-                return Unauthorized();
-            }
+
+            return authUser != null ? Ok(authUser.UserId) : Unauthorized();
         }
     }
 }

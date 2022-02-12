67fc158d32985394d2a4c3ed4d8c688e6ec2893b
lfor <laszlo.forgacs798@gmail.com>
2022-02-12 04:13:17 +0100

    axios api request constructor method added

---
+++ minitwit_admin/src/utils/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import store from '@/compositionStore/index'
+
+const apiURL = process.env.VUE_APP_ADMIN_API_URL
+
+const apiRequest = (method, path, data, contentType) => {
+    store.async.actions.setLoading(true)
+    const url = `${apiURL}${path}`;
+    // TODO: ADD "x-api-key": store.getters.apiKey IF NEEDED LATER
+    const headers = {
+        "Content-Type": "application/json"
+    }
+
+    if  (contentType) {
+        headers["Content-Type"] = contentType
+    }
+
+    return new Promise((resolve, reject) => {
+        const axiosConfig = { method, url, data, headers }
+        return axios(axiosConfig)
+            .then((response) => {
+                resolve(response.data)
+                store.async.actions.setLoading(false)
+            })
+            .catch((error) => {
+                reject(error.response.data)
+                store.async.actions.setError(error.response.data)
+            })
+    })
+}
+
+export {
+    apiRequest
+}

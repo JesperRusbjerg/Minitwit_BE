68d2a71f256328886f0945e491390de5f96ddbdc
lfor <laszlo.forgacs798@gmail.com>
2022-02-12 16:37:36 +0100

    twits store toggleFlag, getTwitList actions and mutations

--- minitwit_admin/src/compositionStore/twits/twitsModule.js
+++ minitwit_admin/src/compositionStore/twits/twitsModule.js
@@ -5,16 +5,50 @@ const state = reactive({
     twitList: []
 })
 
-const actions = {
+const mutations = {
     setTwitList: (twitList) => {
         state.twitList = twitList
     },
 
-    toggleFlag: (messageId, flagAction) => {
-        twitsApi.flagTwit({
-            messageId,
+    updateTwit: (messageId, flagAction) => {
+        const twitIndex = state.twitList.findIndex(twitItem =>
+            twitItem.messageId == messageId
+        )
+        const newTwit = {
+            ...state.twitList[twitIndex],
             flagAction
-        })
+        }
+
+        state.twitList = {
+            ...state.twitList.slice(0, twitIndex),
+            newTwit,
+            ...state.twitList.slice(twitIndex + 1)
+        }
+    }
+}
+
+const actions = {
+    getTwitList: async () => {
+        try {
+            const result = await twitsApi.fetchTwits()
+            mutations.setTwitList(result)
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
+    },
+
+    toggleFlag: async (messageId, flagAction) => {
+        try {
+            await twitsApi.flagTwit({
+                messageId,
+                flagAction
+            })
+            mutations.updateTwit(messageId, !flagAction)
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     }
 }
 

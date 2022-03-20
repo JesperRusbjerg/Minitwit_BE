7695b498bcc0283377dad48dc156e42019d92348
Jesper <jkrusbjerg@gmail.com>
2022-03-20 16:22:41 +0100

    dockerScanTest

---
+++ scripts/dockerSanner.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+docker scan --json --group-issues jrusbjerg/twitbackend > dockerscan.json
+
+if [ -s critical.json];
+then
+  echo "Critical issues found in backend"
+  cat critical.json
+  rm dockerscan.json
+  rm critical.json
+  exit 1
+else
+  echo "No critical issues found in backend"
+  rm dockerscan.json
+  rm critical.json
+fi
+
+docker scan --json --group-issues  snyklabs/tomas-goof > dockerscan.json
+
+if [ -s critical.json];
+then
+  echo "Critical issues found in mariaDB"
+  rm dockerscan.json
+  rm critical.json
+  exit 1
+else
+  echo "No critical issues found in backend"
+  rm dockerscan.json
+  rm critical.json
+fi
+
+
+

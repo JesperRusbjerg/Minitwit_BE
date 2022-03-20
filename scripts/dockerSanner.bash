8c2df23d1b4220c5a662208d6b435f93283285c8
Jesper <jkrusbjerg@gmail.com>
2022-03-20 16:40:04 +0100

    dockerScanTest

--- scripts/dockerSanner.bash
+++ scripts/dockerSanner.bash
@@ -2,10 +2,11 @@
 
 docker scan --json --group-issues jrusbjerg/twitbackend > dockerscan.json
 
-if [ -s dockerscan.json ];
+cat dockerscan.json | grep '"severity": "critical"' > critical.json
+
+if [ -s critical.json ];
 then
   echo "Critical issues found in backend"
-  cat critical.json
   rm dockerscan.json
   rm critical.json
   exit 1
@@ -17,16 +18,19 @@ fi
 
 docker scan --json --group-issues  snyklabs/tomas-goof > dockerscan.json
 
-if [ -s dockerscan.json ];
+cat dockerscan.json | grep '"severity": "critical"' > critical.json
+
+if [ -s critical.json ];
 then
   echo "Critical issues found in mariaDB"
   rm dockerscan.json
+  rm critical.json
   exit 1
 else
   echo "No critical issues found in backend"
   rm dockerscan.json
-  exit 0
+  rm critical.json
 fi
 
 
-
+exit 0

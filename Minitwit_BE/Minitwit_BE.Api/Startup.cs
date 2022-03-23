7ebfeb867fe6f68c5d1fb99673ac85009ed5af47
Szymon Gabryel <szymonso1009@gmail.com>
2022-03-23 22:40:06 +0100

    Fixed small typos in the code.

--- Minitwit_BE/Minitwit_BE.Api/Startup.cs
+++ Minitwit_BE/Minitwit_BE.Api/Startup.cs
@@ -42,17 +42,7 @@ namespace Minitwit_BE.Api
         // to configure HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // Configure the HTTP request pipeline.
-            if (!env.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for
-                // production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseCors("_miniTwitAllowSpecificOrigins");
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             // Prometheus setup start

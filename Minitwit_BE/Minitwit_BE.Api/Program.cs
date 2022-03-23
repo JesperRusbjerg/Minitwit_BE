9da8040f50ed4eb107d4f7894a274827afec103a
Szymon Gabryel <szymonso1009@gmail.com>
2022-03-23 22:29:58 +0100

    Modified solution to take url from appsettings, hidden ports that should not be exposed.

--- Minitwit_BE/Minitwit_BE.Api/Program.cs
+++ Minitwit_BE/Minitwit_BE.Api/Program.cs
@@ -6,17 +6,27 @@ namespace Minitwit_BE.Api
 {
     public class Program
     {
+        private readonly IConfiguration Configuration;
+
+        public Program(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false).Build();
+            var url = config.GetValue<string>("ElasticConfiguration:Uri");
+
             Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
             .Enrich.FromLogContext()
             .WriteTo.Console()
             .WriteTo.File("/home/logs/logs", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10)
-            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(url))
             {
-                IndexFormat="logs",
+                IndexFormat="minitwit-be-logs",
                 AutoRegisterTemplate = true,
                 AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7
             })
